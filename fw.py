ada9902aa9a031df6ba32e253466ce23e9d8b9cf
andrewklayk <andrew.klyachkin@gmail.com>
2023-01-17 14:43:13 +0100

    IT WORKS

--- fw.py
+++ fw.py
@@ -6,16 +6,25 @@ import numpy as np
 # (feasible set consists of probability simplexes)
 # and A - a {0,1} graph adjacency matrix
 
-def maxclique_grad(A: np.ndarray, x: np.ndarray, penalty: str = 'l2', p: int = None, alpha: float = None, beta: float = None, eps: float = None):
-    """
-    Gradient calculation for the Maximal Clique.
+def maxclique_target(A: np.ndarray, x:np.ndarray, p: int=0.5):
+    '''
+    Target function for the Maximal Clique Problem.
+    '''
 
-    A: a R^NxN adjacency matrix;
+    return np.matmul(x.T,np.matmul(A,x)) + p*np.linalg.norm(x)**2
+
+def maxclique_grad(A: np.ndarray, x: np.ndarray, penalty: str = 'l2', p: int = 0.5, alpha: float = None, beta: float = None, eps: float = None):
+    """Gradient calculation for the Maximal Clique Problem.
+    
+    A: a R^NxN symmetric adjacency matrix;
     x: a R^N vector;
     penalty: {'l2', 'f1', 'f2'} string specifying the type of penalty to use; defaults to l2;
     p, alpha, beta, eps: penalty parameters.
+
+    Note: in this problem, the goal is maximization, so the gradient must be multiplied by -1.
     """
-    dp = 2*x
+
+    dp = 2*x*p
     if str.lower(penalty) == 'f1':
         raise NotImplementedError
     if str.lower(penalty) == 'f2':
@@ -23,32 +32,36 @@ def maxclique_grad(A: np.ndarray, x: np.ndarray, penalty: str = 'l2', p: int = N
             raise ValueError
         dp = alpha*np.sum(-beta*np.exp(-beta*x))
 
-    return np.dot((A + A.T), x) + dp
+    return 2 * A @ x + dp
 
 def maxclique_lmo(grad):
     """
     LMO for the maximal clique problem. 
-    The feasible set is the unit simplex, so return a [0,...,1,...,0] vector with 1 in place of the lowest element of the gradient.
+    The feasible set is the unit simplex, so return a [0,...,1,...,0] vector 
+    with 1 in place of the lowest element of the gradient.
     """
+
     e = np.zeros(shape=grad.shape[-1])
     idxmin = np.argmin(grad)
     e[idxmin] = 1
     return e
 
-def frankwolfe(A: np.ndarray, x_0:float, grad: callable=maxclique_grad, lmo: callable=maxclique_lmo, max_iter: int=1000, stepsize: float = None):
+def frankwolfe(x_0: float, grad=maxclique_grad, lmo=maxclique_lmo, max_iter: int = 10000, stepsize: float = None):
     '''
     Basic Frank-Wolfe algorithm.
 
-    A: graph adjacency matrix;
-    g: gradient f-n;
-    stepsize: defaults to 2/(k+2), step defined as (1-stepsize)*x_k + stepsize*x_k_hat;
-    max_iter: max. nr of iterations,
-    x_0: starting point.
+    x_0: starting point;
+    grad: gradient f-n;
+    lmo: linear oracle;
+    max_iter: max. nr of iterations;
+    stepsize: defaults to 2/(k+2), step defined as (1-stepsize)*x_k + stepsize*x_k_hat. Linesearch: TODO;
     '''
+
     x_hist = [x_0]
     s_hist = []
     for k in range(max_iter):
-        s = lmo(grad(A, x_hist[-1]))
+        g = grad(x=x_hist[-1])
+        s = lmo(g)
         gamma = 2/(k+2) if stepsize is None else stepsize
         x_next = (1-gamma)*x_hist[-1] + gamma*s
 
